f6894d89628310e30c0ad4e710813f5cf71759f8
Maximiliano <maximilianofce@gmail.com>
2026-08-24 16:56:49 -0300

    Primer proyecto: calculadora de descuentos

---
+++ calcudescuento.py
@@ -0,0 +1,10 @@
+print("=========================================")
+print("=============CALCUDESCUENTO==============")
+print ("========================================")
+
+monto=float(input("Ingrese solamente el monto al cual se le aplicarà el decuento "))
+descuento=float(input("Ingrese solamente el numero (sin el %) correspondiente al descuento "))
+def calculopreciofinal():
+     return monto*(1-(descuento/100))
+   
+print("el monto que debera abonar luego de que se apicara el desciento sera de $ ", calculopreciofinal())
